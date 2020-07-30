9a75ed2c862f58fbbba2b62f33272032db11eb51
Mateusz Czapliński <mateusz.czaplinski@wpengine.com>
2020-07-30 13:08:36 +0200

    dump all pins

--- cmd/pinner/main.go
+++ cmd/pinner/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 )
 
 const (
@@ -44,6 +47,37 @@ func main() {
 	}
 
 	fmt.Println(string(s))
+
+	////////
+
+	tmp1, err := http.NewRequest(http.MethodGet, "https://api.pinata.cloud/data/pinList", nil)
+	if err != nil {
+		panic(err)
+	}
+	tmp1.Header.Add("Content-Type", "application/json")
+	tmp1.Header.Add("pinata_api_key", api.Key)
+	tmp1.Header.Add("pinata_secret_api_key", api.Secret)
+	// execute the request
+	c := &http.Client{Timeout: 10 * time.Second}
+	tmp2, err := c.Do(tmp1)
+	if err != nil {
+		panic(err)
+	}
+	defer tmp2.Body.Close()
+	tmp3, err := ioutil.ReadAll(tmp2.Body)
+	if err != nil {
+		panic(err)
+	}
+	tmp5 := map[string]interface{}{}
+	err = json.Unmarshal(tmp3, &tmp5)
+	if err != nil {
+		panic(err)
+	}
+	tmp4, err := json.MarshalIndent(tmp5, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(tmp4))
 }
 
 func die(msg ...interface{}) {
