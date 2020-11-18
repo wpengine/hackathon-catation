14971c9a5d93ae0da19d00bc4e53defa6bd12468
Mateusz Czapliński <mateusz.czaplinski@wpengine.com>
2020-11-18 18:38:25 +0100

    xui/xwidget: use older gioui source + apply image resizing

--- xui/xwidget/image.go
+++ xui/xwidget/image.go
@@ -1,11 +1,11 @@
-// FIXME: what license (?) based on: https://git.sr.ht/~eliasnaur/gio/tree/9469d18907629d56d3ed2cdb4ba7fe0f96b2ba0a/widget/image.go
+// FIXME: what license (?) based on: ~/go/pkg/mod/gioui.org@v0.0.0-20200726090339-83673ecb203f/widget/image.go
 
 package xwidget
 
 import (
-	"fmt"
 	"image"
 
+	"gioui.org/f32"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
@@ -28,24 +28,24 @@ func (im Image) Layout(gtx layout.Context) layout.Dimensions {
 	if scale == 0 {
 		scale = 160.0 / 72.0
 	}
-	size := im.Src.Size()
+	size := im.Src.Rect.Size()
 	wf, hf := float32(size.X), float32(size.Y)
 	w, h := gtx.Px(unit.Dp(wf*scale)), gtx.Px(unit.Dp(hf*scale))
 	cs := gtx.Constraints
 	d := cs.Constrain(image.Pt(w, h))
 	stack := op.Push(gtx.Ops)
-
-	resize := float32(d.X) / float32(w)
-	if resizey := float32(d.Y) / float32(h); resizey > resize {
-		resize = resizey
-	}
-	fmt.Println("RESIZE:", resize)
-	// op.Affine((&f32.Affine2D{}).Scale(f32.Pt(0, 0), f32.Pt(resize, resize))).Add(gtx.Ops)
-
 	clip.Rect(image.Rectangle{Max: d}).Add(gtx.Ops)
 
+	// FIXME(akavel): rescaling, but I don't really know what I'm doing here with `ops` :/
+	resize := float32(d.X) / float32(w)
+	if resizey := float32(d.Y) / float32(h); resizey < resize {
+		resize = resizey
+	}
+	d = image.Pt(int(float32(w)*resize), int(float32(h)*resize))
+	op.Affine((&f32.Affine2D{}).Scale(f32.Pt(0, 0), f32.Pt(resize, resize))).Add(gtx.Ops)
+
 	im.Src.Add(gtx.Ops)
-	paint.PaintOp{}.Add(gtx.Ops)
+	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: float32(w), Y: float32(h)}}}.Add(gtx.Ops)
 	stack.Pop()
 	return layout.Dimensions{Size: d}
 }
